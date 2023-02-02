7a841512437bc941f844cdea52419074b09f7c1c
ambient3211 <1adrianrus1@gmail.com>
2023-02-02 16:54:52 +0200

    First change is made

--- src/com/company/DevTestClass.java
+++ src/com/company/DevTestClass.java
@@ -3,5 +3,7 @@ package com.company;
 public class DevTestClass {
     public static void main(String[] args) {
         System.out.println("DevClassTest Created!");
+
+        //Some changes made here
     }
 }
